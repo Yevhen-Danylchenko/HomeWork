d7144dd045d99cd81d97d5fc90865c88243e5123
Yevhen <yevhen_danylchenko@ukr.net>
2024-07-03 21:52:05 +0300

    My home work

---
+++ MaxNumber.py
@@ -0,0 +1,10 @@
+numbers = [1, -2, 3, -4, 0, 6, 337, -108, 49, 0]
+
+maxNumber = 0
+
+for index in numbers :
+    if maxNumber <= index :
+
+        maxNumber = index
+
+print(f'Найбільше число в цьому списку дорівнює: {maxNumber} \n')
---
+++ Number.py
@@ -0,0 +1,11 @@
+numbers = [1, -2, 3, -4, 0, 6, 337, -108, 49, 0]
+
+evenNumbers = []
+
+for index in range(len(numbers)) :
+
+    if numbers[index] % 2 == 0 :
+
+      print(numbers[index])
+
+
